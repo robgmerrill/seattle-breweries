fdcfa443e5d2d52776f4614c20f5ce56ea101bc8
Rob Merrill <robgmerrill@gmail.com>
2016-04-06 11:49:00 -0700

    express server setup

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-require('./instantHello');
-var goodbye = require('./talk/goodbye');
-var talk = require('./talk');
+var express = require('express');
+var app = express();
 
-talk.intro();
-talk.hello('Rob');
+app.set('port', 3000);
 
-
-goodbye();
+app.listen(app.get('port'));
+console.log('Magic happens on port ' + app.get('port'));
