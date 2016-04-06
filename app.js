b3dc270b247eb538a602bd0347d12405ca57128c
Rob Merrill <robgmerrill@gmail.com>
2016-04-06 12:38:43 -0700

    express rendering for index.html

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.set('port', 3000);
 
-app.listen(app.get('port'));
-console.log('Magic happens on port ' + app.get('port'));
+app.get('/', function(req, res) {
+  console.log('GET the homepage');
+  res
+    .status(200)
+    .sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/json', function(req, res) {
+  console.log('GET the json');
+  res
+    .status(200)
+    .send( {"jsonData": true} );
+});
+
+app.get('/file', function(req, res) {
+  console.log('GET the file');
+  res
+    .status(200)
+    .sendFile(path.join(__dirname, 'app.js'));
+});
+
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
+});
+
