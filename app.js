33ab407747a052c3635942a001327b343057f5c3
Rob Merrill <robgmerrill@gmail.com>
2016-05-15 13:03:19 -0700

    added first app.use middleware and updated index.html

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 var express = require('express');
-var app = express();
+var app = express(); // extantiate express
 var path = require('path');
 
 app.set('port', 3000);
 
-app.get('/', function(req, res) {
-  console.log('GET the homepage');
-  res
-    .status(200)
-    .sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// middleware will serve up all the static content in the public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
+// http method get as basic route for the home page
+// req and res are the typical parameters for the callback funtion
+// app.get('/', function(req, res) {
+//   console.log('GET the homepage');
+//   res
+//     .status(200)
+    // .sendFile method. Pass the name of the file you want to send
+    // needs the native node module named "path"
+    // use path.join to create file path, __dirname finds current directory, public for folder, name the file
+//     .sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+// route /json
 app.get('/json', function(req, res) {
   console.log('GET the json');
   res
@@ -18,10 +27,14 @@ app.get('/json', function(req, res) {
     .send( {"jsonData": true} );
 });
 
+// route /file
 app.get('/file', function(req, res) {
   console.log('GET the file');
   res
+    // status of 200 means ok
     .status(200)
+    // required "path" to use .sendFile function. 
+    // formula: (path.join(__dirname, 'file'))
     .sendFile(path.join(__dirname, 'app.js'));
 });
 
